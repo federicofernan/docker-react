2ccc421eb6f2bcdff91714ff8be957845361c5a5
Federico Fernandez <federicofernandez@Federicos-MBP.hsd1.fl.comcast.net>
2020-07-05 14:38:12 -0400

    changed the text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! This is a React.Js Application
+          Hello World! This is a React.Js Application - New Feature
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Changed for Feature Branch
+          Learn React
         </a>
       </header>
     </div>
